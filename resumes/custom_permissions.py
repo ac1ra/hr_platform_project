1a59ca887c5b97f9ae97cf3570cf29065e809ad5
ac1ra <glombinski@gmail.com>
2025-10-22 16:58:00 +0300

    update files

---
+++ resumes/custom_permissions.py
@@ -0,0 +1,14 @@
+from rest_framework import permissions
+
+class IsOwnerOrAdmin(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        try:
+            if not request.user.is_authenticated:
+                return False
+            if hasattr(obj, "user"):
+                return obj.user == request.user
+            if hasattr(obj, "user_id"):
+                return obj.user_id == request.user
+            return request.user.is_superuser
+        except Exception:
+            return False
--- resumes/views.py
+++ resumes/views.py
@@ -1,10 +1,23 @@
-from rest_framework import viewsets
+# from rest_framework import viewsets
+from rest_framework import viewsets,permissions
+from .custom_permissions import IsOwnerOrAdmin
 from .models import Resume
 from .serializers import ResumeSerializer
 
 class ResumeViewSet(viewsets.ModelViewSet):
     queryset = Resume.objects.all()
     serializer_class = ResumeSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
-    def perform_create(self, serializer):
-        serializer.save(user=self.request.user)
+    def get_permissions(self):
+        if self.action != "create":
+            return [IsOwnerOrAdmin()]
+        return super().get_permissions()
+    
+    def get_queryset(self):
+        try:
+            if not self.request.user.is_staff or not self.request.user.is_superuser:
+                return Resume.objects.filter(user_id=self.request.user)
+        except Exception:
+            return Resume.objects.none()
+        return super().get_queryset()
